f16a6e0dd7dac5d0148b9561d238a7ae29e06ab9
Balvinder Singh <balvinder294@gmail.com>
2026-02-13 18:55:53 +0530

    crypto changes

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
       name: 'quest-node-alpha',
       script: 'witness-node.js',
       // Decodes to exactly 64 bytes (88 chars Base64)
-      args: '--port 8089 --name tekraze --key QUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQUFBQQ== --db alpha --peers ws://localhost:8096,ws://localhost:8097,wss://wsgaming.blurt.one',
+      args: '--port 8089 --name tekraze --key OqajXNQQG9GIAk4Yr2hWTdWckC9ULJ+A/2hrbRKdiJlm5Q/Kqx/4u8Ud1dvjNlfLLq6eYV4MpJ5iC9HIvy2edA== --db alpha --peers ws://localhost:8096,ws://localhost:8097,wss://wsgaming.blurt.one',
       watch: false,
       max_memory_restart: '512M',
       env: {
@@ -16,7 +16,7 @@ module.exports = {
       name: 'quest-node-beta',
       script: 'witness-node.js',
       // Decodes to exactly 64 bytes (88 chars Base64)
-      args: '--port 8096 --name kamranrkploy --key QkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkJCQkI== --db beta --peers ws://localhost:8089,ws://localhost:8097,wss://wsgaming.blurt.one',
+      args: '--port 8096 --name kamranrkploy --key N6jhE01pjosB1lQ7QI9n2Np2DK8+tNq+ukw1AuMmcUxFU6c+Q0gyFpLDSDVGOX//Rt/5kGtFx4pKzBGkOgACVw== --db beta --peers ws://localhost:8089,ws://localhost:8097,wss://wsgaming.blurt.one',
       watch: false,
       max_memory_restart: '512M',
       env: {
@@ -27,7 +27,7 @@ module.exports = {
       name: 'quest-node-gamma',
       script: 'witness-node.js',
       // Decodes to exactly 64 bytes (88 chars Base64)
-      args: '--port 8097 --name node_gamma --key Q0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0NDQ0M== --db gamma --peers ws://localhost:8089,ws://localhost:8096,wss://wsgaming.blurt.one',
+      args: '--port 8097 --name node_gamma --key 8xXyUWy9cSbsvYuKatlXxvERBB9MEaeo4CLVPrWtbX1aI23aVlc8j1xi57AS+zSDMrqyY45S/XdBLR7jo4oWow== --db gamma --peers ws://localhost:8089,ws://localhost:8096,wss://wsgaming.blurt.one',
       watch: false,
       max_memory_restart: '512M',
       env: {
